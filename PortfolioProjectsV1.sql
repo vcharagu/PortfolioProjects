20392d6f167c1f8b5c2a8feb2417c41a674fbec5
vcharagu <33426484+vcharagu@users.noreply.github.com>
2022-12-19 20:07:15 +0300

    Add files via upload

---
+++ PortfolioProjectsV1.sql
@@ -0,0 +1,12 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[RollingPeopleVaccinated]
+  FROM [mememe].[dbo].[PercentagePopulationVaccinated]
+
+
+select *
+from PercentagePopulationVaccinated
